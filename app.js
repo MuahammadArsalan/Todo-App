f414428e1246156e71e4a773d3d986f91237feab
MuhammadArsalan <muhammadarsalanforever@gmail.com>
2024-03-21 13:09:23 +0530

    Todo App Completed

--- app.js
+++ app.js
@@ -1,73 +1,138 @@
-// const todoInput = document.querySelector('#todo');
-// const ol = document.querySelector('ol');
-// const arr = [];
-
-// function addTodo() {
-//     const todo = todoInput.value;
-//     arr.push(todoInput.value);
-//     console.log(arr);
-//     ol.innerHTML = '';
-//     for (let i = 0; i < arr.length; i++) {
-//         ol.innerHTML += `<li>
-//     ${arr[i]}
-//     <button onclick="deleteTodo(${i}) ">delete</button> 
-//     <button onclick="editTodo(${i})">edit Todo</button>
-//     </li>   `
-//     }
-//     todoInput.value = ''
-// }
-// function deleteTodo(index) {
-//     console.log('deleteTodo', index);
-// }
-// function editTodo(index) {
-//     console.log('editTodo', index);
-// }
-
-
-// function render() {
-//     arr.splice(index, 1);
-
-// }
-
-
-
-
-
-
 const todoInput = document.querySelector('#todo');
 const ol = document.querySelector('ol');
-
 const arr = [];
 
-function addTodo(){
 
+function render(){
+  ol.innerHTML = '';
+ 
+
+  for (let i =0 ;i<arr.length;i++){
+      ol.innerHTML += `<li>${arr[i]} 
+      <button onclick ="deleteTodo(${[i]})"  class="delete">Delete</button>
+      <button onclick ="editTodo(${[i]})"  class="edit">Edit</button>
+      </li>`
+    }
+
+}
+
+
+
+function addTodo(){
   arr.push(todoInput.value);
   console.log(arr);
-  ol.innerHTML = ''
-  for(let i=0;i<arr.length;i++){
-ol.innerHTML += `<li>${arr[i]} 
-<button onclick="deleteTodo(${i})" class="delete">Delete</button>
-<button onclick="editTodo(${i})" class="edit">Edit</button>
-</li>`
-  }
+ol.innerHTML = '';
+ 
 
+for (let i =0 ;i<arr.length;i++){
+    ol.innerHTML += `<li>${arr[i]} 
+    <button onclick ="deleteTodo(${[i]})" class="delete">Delete</button>
+    <button onclick ="editTodo(${[i]})" class="edit">Edit</button>
+    </li>`
+  }
   todoInput.value = '';
 }
 
+
 function deleteTodo(index){
 
-// console.log('Delete todo and the index number is ===>',index);
-// ol.innerHTML =''
-
+console.log('delete todo is ',index);
+arr.splice(index,1);
+render()
 
 }
+
 function editTodo(index){
-    // console.log('Edit todo and the index number is ===>',index);
-    let update = prompt('Enter updated value');
+ const updatedVal= prompt('Enter updated value');
+    console.log('edit todo is ',index);
+  arr.splice(index,1,updatedVal);
+render()
+
 }
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const todoInput = document.querySelector('#todo');
+// const ol = document.querySelector('ol');
+
+// const arr = [];
+
+
+// function addTodo(){
+// ol.innerHTML = '';
+//   arr.push(todoInput.value);
+//   console.log(arr);
+
+//    for(let i=0;i<arr.length;i++){
+//   ol.innerHTML += `<li>${arr[i]} 
+//   <button onclick="deleteTodo(${i})" class="delete">Delete</button>
+//   <button onclick="editTodo(${i})" class="edit">Edit</button>
+//   </li>`
+//   }
+
+//   todoInput.value = '';
+// }
+
+// function deleteTodo(index){
+
+// console.log('The index number is ', index);
+// arr.splice(index,1);
+// ol.innerHTML ='';
+// for(let i=0;i<arr.length;i++){
+//   ol.innerHTML += `<li>${arr[i]} 
+//   <button onclick="deleteTodo(${i})" class="delete">Delete</button>
+//   <button onclick="editTodo(${i})" class="edit">Edit</button>
+//   </li>`
+//   }
+
+
+
+// }
+// function editTodo(index){
+  
+// console.log('The index number is ', index);
+// const updatedVal = prompt('Enter the updated value')
+// arr.splice(index,1,updatedVal);
+
+// ol.innerHTML = '';
+// for(let i=0;i<arr.length;i++){
+//   ol.innerHTML += `<li>${arr[i]} 
+//   <button onclick="deleteTodo(${i})" class="delete">Delete</button>
+//   <button onclick="editTodo(${i})" class="edit">Edit</button>
+//   </li>`
+//   }
+
+// }
 
 
 
